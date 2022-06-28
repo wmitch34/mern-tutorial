d023b09613986f4d2d88b59923f84659e16611e4
Will Mitchell <53200009+wmitch34@users.noreply.github.com>
2022-06-28 16:43:41 -0400

    fixed .env routing issue

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require ('express')
-const dotenv = require ('dotenv').config
-const port = process.env.PORT || 5000
+const dotenv = require ('dotenv').config()
+const port = process.env.PORT
 
 const app = express()
 app.use('/api/goals', require('./routes/goalRoutes'))
